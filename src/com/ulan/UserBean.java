50fa519af629fcaf82965057cfbd9715bac8f802
Ulan <Ulan@Ulan-PC>
2015-02-11 11:47:14 +0600

    f

--- src/com/ulan/UserBean.java
+++ src/com/ulan/UserBean.java
@@ -3,9 +3,10 @@ package com.ulan;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name="bean1")
-@RequestScoped
+@ViewScoped
 public class UserBean {
 	
 	private String name;
